6d7b5d8869e573bc440576a8dc80d816ac631088
Sergey Abramkin <abramkins@gmail.com>
2019-02-26 16:39:10 +0000

    feat: add grpc dynamic idl generation

---
+++ grpc/elliptics_generated_wrapper.cpp
@@ -0,0 +1,5 @@
+// This file proxies another generated file. Since generated can be non existent, it cannot
+// be used as target dependency. Current file permanently exists and used as dependency
+// TODO: get out this hack, use cmake native manner to work with generated
+
+#include "elliptics.grpc.fb.cc"
